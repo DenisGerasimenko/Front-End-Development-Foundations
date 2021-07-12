c1c98712479f0e4b1459925c55a677860b29be89
Dzianis Herasimenka <Dzianis_Herasimenka@epam.com>
2021-07-12 15:51:26 +0300

    add asynchronous1 & asynchronous2

---
+++ asynchronous1.js
@@ -0,0 +1,15 @@
+1. const MyPromise = class {
+    constructor (callback) {this.promise = new Promise (callback)}
+    then (fn) {this.promise.then(fn); return this}
+    synchThen (fn) {fn(); return this}
+}
+
+const promise = new MyPromise((resolve) => {
+    console.log(1)
+    resolve()
+}).synchThen(() => {
+    console.log(2)
+}).then(() => {
+    console.log(3)
+})
+console.log(4)
