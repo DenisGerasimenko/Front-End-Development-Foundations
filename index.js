15c0e16234420eaa2dc13a4740aef8f96c267037
Dzianis Herasimenka <Dzianis_Herasimenka@epam.com>
2021-07-12 12:45:57 +0300

    delete idea

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function NamedOne(first, last){
 
 NamedOne.prototype = {
     get fullName(){
-        return this.firstName+' '+this.lastName;
+        return ${this.firstName} ${this.lastName};
     },
     set fullName(fn){
         var name = fn.split(' ');
@@ -15,3 +15,4 @@ NamedOne.prototype = {
         }
     }
 }
+
