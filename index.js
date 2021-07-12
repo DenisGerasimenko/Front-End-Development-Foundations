3bb2a1fba23a1a6785cb7e1af976515c681f4c6f
Dzianis Herasimenka <Dzianis_Herasimenka@epam.com>
2021-07-12 11:47:58 +0300

    add homeTask

---
+++ index.js
@@ -0,0 +1,17 @@
+function NamedOne(first, last){
+    this.firstName = first;
+    this.lastName = last;
+}
+
+NamedOne.prototype = {
+    get fullName(){
+        return this.firstName+' '+this.lastName;
+    },
+    set fullName(fn){
+        var name = fn.split(' ');
+        if(name.length == 2){
+            this.firstName = name[0];
+            this.lastName  = name[1];
+        }
+    }
+}
