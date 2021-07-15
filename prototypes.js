72621d4f8073b57a319a52f6665783a69a46548f
DenisGerasimenko <61081562+DenisGerasimenko@users.noreply.github.com>
2021-07-15 18:21:47 +0300

    Update prototypes.js

--- prototypes.js
+++ prototypes.js
@@ -2,13 +2,13 @@ function myNew(Constructor, ...arg) {
     let obj = Object.create(Constructor.prototype);
     let result = Constructor.apply(obj, [...arg]);
 
-    if ( typeof result == 'undefined') {
+    if ( typeof result === 'undefined') {
         return obj;
-    } else if (typeof result == 'string') {
+    } else if (typeof result === 'string') {
         return obj;
-    } else if (typeof result == 'boolean') {
+    } else if (typeof result === 'boolean') {
         return obj;
-    } else if (result == null) {
+    } else if (result === null) {
         return obj;
     } else {
         return result;
