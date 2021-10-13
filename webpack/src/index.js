7c9c9f0c633346601c6f73c1cf3c39f317709d95
Dzianis Herasimenka <Dzianis_Herasimenka@epam.com>
2021-10-13 13:39:54 +0300

    settings for webpack

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,1 +1,20 @@
-console.log("Как интересно!");
+
+import './styles/main.scss'
+// создание свойства класса без конструктора
+class Game {
+    name = 'Violin Charades'
+}
+
+const myGame = new Game();
+
+//создаем параграф
+const p = document.createElement('p')
+p.textContent = 'I like ${myGame.game}.'
+
+//создаем элемент заголовка
+const heading = document.createElement('h1');
+heading.textContent = 'Как интересно!';
+
+// добавляем заголовок в DOM
+const root = document.querySelector('#root');
+root.append(heading);
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const webpack = require('webpack')
+
 
 module.exports = {
     entry: {
@@ -8,4 +12,43 @@ module.exports = {
         path: path.resolve(__dirname, './dist'),
         filename: '[name].bundle.js',
     },
+    mode: 'development',
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.resolve(__dirname, './dist'),
+        open: true,
+        compress: true,
+        hot: true,
+        port: 8080,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "webpack Boilerplate",
+            template: path.resolve(__dirname, "./src/template.html"),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test:/\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+            },
+        ]
+    }
 }
